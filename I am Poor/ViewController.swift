ccead168943f07544d0392f95fd0dcf843e9b60a
Salvatore-2512 <aryanvasudeva@icloud.com>
2023-11-17 13:09:53 +0530

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Aryan Vasudeva on 17/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
